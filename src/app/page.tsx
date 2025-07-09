1a42c0726dfd00c9c7311c53762173c86e2951e0
Lechcher <lechcher@mail.com>
2025-07-09 16:29:40 +0700

    HomePage part 1: CompanionCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,45 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <>
-      <h1 className=" text-2xl  underline">Welcome to my SAAS App</h1>
-      <Button>Let&apos;s get started</Button>
-    </>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id={123}
+          name="Neura the Brainy Explorer"
+          topic="Topic: Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id={125}
+          name="Countsy the Number Wizard"
+          topic="Topic: Derivatives & IntegralsDerivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id={124}
+          name="Verba the Vocabulary Builder"
+          topic="Topic: English Literature"
+          subject="Language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
